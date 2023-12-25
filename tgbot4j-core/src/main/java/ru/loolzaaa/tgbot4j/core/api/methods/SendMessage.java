feaa7492f9864eadf5ae284fcc07825dd00be4b6
Andrey Korsakov <loolzaaa@gmail.com>
2023-12-25 23:27:15 +0500

    Add SendMessage method

---
+++ tgbot4j-core/src/main/java/ru/loolzaaa/tgbot4j/core/api/methods/SendMessage.java
@@ -0,0 +1,107 @@
+package ru.loolzaaa.tgbot4j.core.api.methods;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.loolzaaa.tgbot4j.core.api.JsonResponseDeserializer;
+import ru.loolzaaa.tgbot4j.core.api.types.Message;
+import ru.loolzaaa.tgbot4j.core.api.types.MessageEntity;
+import ru.loolzaaa.tgbot4j.core.api.types.ReplyMarkup;
+
+import java.util.List;
+
+/**
+ * Use this method to send text messages.
+ * On success, the sent {@link Message} is returned.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SendMessage implements JsonResponseDeserializer<Message> {
+    /**
+     * Unique identifier for the target chat or username
+     * of the target channel (in the format {@code @channelusername})
+     */
+    @JsonProperty("chat_id")
+    private String chatId;
+
+    /**
+     * Unique identifier for the target message thread (topic)
+     * of the forum; for forum supergroups only
+     */
+    @JsonProperty("message_thread_id")
+    private Integer messageThreadId;
+
+    /**
+     * Text of the message to be sent,
+     * 1-4096 characters after entities parsing
+     */
+    @JsonProperty("text")
+    private String text;
+
+    /**
+     * Mode for parsing entities in the message text.
+     * See <a href="https://core.telegram.org/bots/api#formatting-options">formatting options</a> for more details.
+     */
+    @JsonProperty("parse_mode")
+    private String parseMode;
+
+    /**
+     * A JSON-serialized list of special entities
+     * that appear in message text,
+     * which can be specified instead of parse_mode
+     */
+    @JsonProperty("entities")
+    private List<MessageEntity> entities;
+
+    /**
+     * Disables link previews for links in this message
+     */
+    @JsonProperty("disable_web_page_preview")
+    private Boolean disableWebPagePreview;
+
+    /**
+     * Sends the message <a href="https://telegram.org/blog/channels-2-0#silent-messages">silently</a>.
+     * Users will receive a notification with no sound.
+     */
+    @JsonProperty("disable_notification")
+    private Boolean disableNotification;
+
+    /**
+     * Protects the contents of the sent message
+     * from forwarding and saving
+     */
+    @JsonProperty("protect_content")
+    private Boolean protectContent;
+
+    /**
+     * If the message is a reply, ID of the original message
+     */
+    @JsonProperty("reply_to_message_id")
+    private Integer replyToMessageId;
+
+    /**
+     * Pass True if the message should be sent
+     * even if the specified replied-to message is not found
+     */
+    @JsonProperty("allow_sending_without_reply")
+    private Boolean allowSendingWithoutReply;
+
+    /**
+     * Additional interface options.
+     * A JSON-serialized object for an <a href="https://core.telegram.org/bots/features#inline-keyboards">inline keyboard</a>,
+     * <a href="https://core.telegram.org/bots/features#keyboards">custom reply keyboard</a>, instructions to remove reply keyboard
+     * or to force a reply from the user.
+     */
+    @JsonProperty("reply_markup")
+    private ReplyMarkup replyMarkup;
+
+    @Override
+    public Message determineResponseType(ObjectMapper mapper, JsonNode resultNode) {
+        return deserializeObjectResponse(mapper, resultNode, Message.class);
+    }
+}
