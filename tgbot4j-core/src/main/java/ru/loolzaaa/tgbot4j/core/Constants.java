ad66e51b393d7ff02c82bf339e47dba187948f73
Korsakov A.N. (20211) <loolzaaa@gmail.com>
2023-12-25 13:18:17 +0500

    Add common constants

---
+++ tgbot4j-core/src/main/java/ru/loolzaaa/tgbot4j/core/Constants.java
@@ -0,0 +1,9 @@
+package ru.loolzaaa.tgbot4j.core;
+
+public class Constants {
+    public static final String BASE_URL = "https://api.telegram.org/bot";
+
+    public static final String CHARSET_HEADER = "Charset";
+    public static final String CONTENT_TYPE_HEADER = "Content-Type";
+    public static final String JSON_CONTENT_TYPE_VALUE = "application/json";
+}
