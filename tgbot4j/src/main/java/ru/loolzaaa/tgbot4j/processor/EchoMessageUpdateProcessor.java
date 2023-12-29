6b4aa76a9486ef60f8c567327e9f01f79073f026
Korsakov A.N. (20211) <loolzaaa@gmail.com>
2023-12-29 10:15:34 +0500

    Create test echo update processor

---
+++ tgbot4j/src/main/java/ru/loolzaaa/tgbot4j/processor/EchoMessageUpdateProcessor.java
@@ -0,0 +1,27 @@
+package ru.loolzaaa.tgbot4j.processor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.loolzaaa.tgbot4j.core.api.methods.SendMessage;
+import ru.loolzaaa.tgbot4j.core.api.types.Message;
+import ru.loolzaaa.tgbot4j.core.api.types.Update;
+import ru.loolzaaa.tgbot4j.core.processor.UpdateProcessor;
+import ru.loolzaaa.tgbot4j.core.processor.UpdateProcessorChain;
+import ru.loolzaaa.tgbot4j.core.sender.MethodSender;
+
+public class EchoMessageUpdateProcessor implements UpdateProcessor {
+
+    private static final Logger log = LoggerFactory.getLogger(EchoMessageUpdateProcessor.class);
+
+    @Override
+    public void process(Update update, MethodSender methodSender, UpdateProcessorChain chain) {
+        if (update.getMessage() != null) {
+            SendMessage sendMessage = new SendMessage();
+            sendMessage.setChatId(update.getMessage().getChat().getId().toString());
+            sendMessage.setText(update.getMessage().getText());
+            Message send = methodSender.send(sendMessage);
+            log.info("Echo message: {}", send);
+        }
+        chain.doProcess(update, methodSender);
+    }
+}
